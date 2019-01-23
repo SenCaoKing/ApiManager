e318ed0c9c414e928f61bd6cff605c84c280966f
SenCaoKing <1134349927@qq.com>
2019-01-23 22:07:21 +0800

    修改各页面信息

--- MinPHP/run/login.php
+++ MinPHP/run/login.php
@@ -1,8 +1,30 @@
 <!-- 登录与退出 start -->
 <?php defined('API') or exit('https://baidu.com');?>
 <?php
-$type = $_GET['type'];
+$type = I($_GET['type']);
+// 登录
+if($type == 'do'){
+    $_VAL = I($_POST);
+    
+}
 
-var_dump($type);
+
+
 ?>
+<div style="border: 1px solid #ddd;">
+    <div style="background: #f5f5f5; padding: 20px; position: relative;">
+        <h4>登录</h4>
+        <div>
+            <form action="?act=login&type=do" method="post">
+                <div class="form-group">
+                    <input type="text" class="form-control" name="name" placeholder="登录名">
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="pwd" placeholder="密码">
+                </div>
+                <button class="btn btn-success">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
 <!-- 登录与退出 end -->
