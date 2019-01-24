c2d3843196d23e20a9b390e393ab8b7cbd6df34b
SenCaoKing <1134349927@qq.com>
2019-01-24 20:16:12 +0800

    分类修改、删除

--- MinPHP/run/cate.php
+++ MinPHP/run/cate.php
@@ -4,7 +4,6 @@
 if(!is_supper()){ die('只有超级管理员才可对分类进行操作'); }
 // 操作类型(add, delete, edit)
 $op = I($_POST['op']);
-var_dump($op);
 // 是否执行操作(如果为do的话，则为执行添加，删除，编辑操作)
 $type= I($_GET['type']);
 switch($op){
@@ -28,13 +27,35 @@ switch($op){
             }
         }
         break;
+    // 删除
+    case 'delete':
+        $aid = I($_POST['aid']);
+        $sql = "update cate set isdel=1 where aid='{$aid}'";
+        $re = update($sql);
+        if($re){
+            go(U());
+        }else{
+            echo '<div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> 分类删除失败</div>';
+        }
+        break;
+    // 编辑
     case 'edit':
         $_VAL = I($_POST);
-
-        dump($_VAL);
+        if($type == 'do'){
+            $sql = "update cate set cname='{$_VAL['cname']}', cdesc='{$_VAL['cdesc']}' where aid='{$_VAL['aid']}'";
+            $re = update($sql);
+            if($re !== false){
+                go(U());
+            }else{
+                echo '<div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> 分类修改失败</div>';
+            }
+        }else{
+            $aid = $_VAL['aid'];
+            $sql = "select * from cate where aid='{$aid}'";
+            $info = find($sql);
+        }
         break;
 }
-
 ?>
 <?php if($op == 'add'){ ?>
 <div style="border: 1px solid #ddd;">
@@ -53,4 +74,22 @@ switch($op){
         </div>
     </div>
 </div>
+<?php }if($op == 'edit'){ ?>
+    <div style="border: 1px solid #ddd;">
+        <div style="background: #f5f5f5; padding: 20px; position: relative;">
+            <h4>编辑分类</h4>
+            <div>
+                <form action="?act=cate&type=do" method="post">
+                    <div class="form-group">
+                        <input type="hidden" name="aid" value="<?php echo $info['aid']?>">
+                        <input type="text" class="form-control" name="cname" placeholder="分类名" value="<?php echo $info['cname']?>">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="cdesc" placeholder="描述" value="<?php echo $info['cdesc']?>">
+                    </div>
+                    <button class="btn btn-success" name="op" value="edit">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
 <?php } ?>
--- MinPHP/run/menu.php
+++ MinPHP/run/menu.php
@@ -35,44 +35,45 @@
         </ul>
     </div>
 
-<form action="?act=cate" method="post">
+    <form action="?act=cate" method="post">
+        <?php if(is_supper()){ ?>
+            <!-- 只有超级管理员才可以添加分类 -->
+            <div style="float: right; margin-right: 20px;">
+                <button class="btn btn-success" name="op" value="add">新建分类</button>
+            </div>
+        <?php } ?>
+    </form>
+<?php } else { ?>
 
 
-    <div style="float: right; margin-right: 20px;">
-        <button class="btn btn-success" name="op" value="add">新建分类</button>
+    <div class="form-group">
+        <input type="text" class="form-control" id="searchapi" placeholder="search here" onkeyup="  ">
     </div>
+    <div class="list">
+        <ul class="list-unstyled" style="padding: 10px;">
 
-</form>
+            <li class="menu" id="api ">
+                <a href=" ">
+                    <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+
+                </a>
+            </li>
+
+            <span class="keyword" id=" "></span>
+
+
+        </ul>
+    </div>
+    <form action="?act=api " method="post">
+
+
+        <div style="float: right; margin-right: 20px;">
+            <input type="hidden" value="" name="aid">
+            <button class="btn btn-success">新建接口</button>
+        </div>
+
+    </form>
 <?php } ?>
-
-
-<div class="form-group">
-    <input type="text" class="form-control" id="searchapi" placeholder="search here" onkeyup="">
-</div>
-<div class="list">
-    <ul class="list-unstyled" style="padding: 10px;">
-
-        <li class="menu" id="api ">
-            <a href=" ">
-                <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
-
-            </a>
-        </li>
-
-        <span class="keyword" id=" "></span>
-
-
-    </ul>
-</div>
-<form action="?act=api " method="post">
-
-
-    <div style="float: right; margin-right: 20px;">
-        <input type="hidden" value="" name="aid">
-        <button class="btn btn-success">新建接口</button>
-    </div>
-
-</form>
 
 <!-- jquery  -->
 <script type="text/javascript">
